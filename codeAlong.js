bf9ebfc0723aad7d2da5d7ffe8289d9e83eab484
samfrexz <samueloyawale315@gmail.com>
2021-03-17 10:35:11 +0100

    second commit

--- codeAlong.js
+++ codeAlong.js
@@ -2,4 +2,7 @@
 const headerTitle = document.querySelector('.title');
 headerTitle.innerText = 'hello world';
 
-
+const items = document.getElementsByClassName('list-group-item');
+items[1].textContent = 'welcome Samfrexz';
+items[1].style.backgroundColor = '#f4f4f4';
+items[1].style.color = 'red';
